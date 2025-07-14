aa07fa738b425d6456cd75f8c47fed4857aebd7e
Alnick Clores <aelnickclores@gmail.com>
2025-07-14 23:38:08 +0800

    Implemented a logout button that clears the token and redirects to login and registration page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { validateToken } from "../services/authService";
 import { getApplications } from "../services/applicationService";
 import { Application } from "../types";
+import { LogOut } from "lucide-react";
 
 interface DashboardData {
   message: string;
@@ -37,12 +38,31 @@ const DashboardPage = () => {
     fetchData();
   }, []);
 
+  const handleLogout = () => {
+    const logoutConfirmed = window.confirm("Are you sure you want to logout?");
+
+    if (logoutConfirmed) {
+      sessionStorage.clear();
+      router.push("/");
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 via-white to-purple-100">
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold text-purple-600 mb-5">
-          Hi, {data?.user.username}!
-        </h1>
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-bold text-purple-600 mb-5">
+            Hi, {data?.user.username}!
+          </h1>
+          <div>
+            <button
+              onClick={handleLogout}
+              className="flex gap-1 items-center w-full sm:w-auto bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-md text-sm font-medium transition-colors duration-200"
+            >
+              Logout <LogOut className="w-4 h-4" />
+            </button>
+          </div>
+        </div>
         <div className="mb-8">
           <h1 className="text-3xl font-bold text-gray-900 mb-2">
             Job Applications
